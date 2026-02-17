b69902efe33cc45066a33c40648a05388c5bc0f8
ZeCrusher <102222839+ZeCrusher@users.noreply.github.com>
2026-02-17 12:17:32 +0100

    Update index.php with new header and formatting
    Ajout du Logo Zerg ^^'

--- index.php
+++ index.php
@@ -1,12 +1,87 @@
 <?php
 /* 		 
-					MMM"""AMV `7MM"""YMM    .g8"""bgd `7MM"""Mq.`7MMF'   `7MF'.M"""bgd `7MMF'  `7MMF'`7MM"""YMM  `7MM"""Mq.  
-					M'   AMV    MM    `7  .dP'     `M   MM   `MM. MM       M ,MI    "Y   MM      MM    MM    `7    MM   `MM. 
-					'   AMV     MM   d    dM'       `   MM   ,M9  MM       M `MMb.       MM      MM    MM   d      MM   ,M9  
-					   AMV      MMmmMM    MM            MMmmdM9   MM       M   `YMMNq.   MMmmmmmmMM    MMmmMM      MMmmdM9   
-					  AMV   ,   MM   Y  , MM.           MM  YM.   MM       M .     `MM   MM      MM    MM   Y  ,   MM  YM.   
-					 AMV   ,M   MM     ,M `Mb.     ,'   MM   `Mb. YM.     ,M Mb     dM   MM      MM    MM     ,M   MM   `Mb. 
-					AMVmmmmMM .JMMmmmmMMM   `"bmmmd'  .JMML. .JMM. `bmmmmd"' P"Ybmmd"  .JMML.  .JMML..JMMmmmmMMM .JMML. .JMM.
+																MMM"""AMV `7MM"""YMM    .g8"""bgd `7MM"""Mq.`7MMF'   `7MF'.M"""bgd `7MMF'  `7MMF'`7MM"""YMM  `7MM"""Mq.  
+																M'   AMV    MM    `7  .dP'     `M   MM   `MM. MM       M ,MI    "Y   MM      MM    MM    `7    MM   `MM. 
+																'   AMV     MM   d    dM'       `   MM   ,M9  MM       M `MMb.       MM      MM    MM   d      MM   ,M9  
+																   AMV      MMmmMM    MM            MMmmdM9   MM       M   `YMMNq.   MMmmmmmmMM    MMmmMM      MMmmdM9   
+																  AMV   ,   MM   Y  , MM.           MM  YM.   MM       M .     `MM   MM      MM    MM   Y  ,   MM  YM.   
+																 AMV   ,M   MM     ,M `Mb.     ,'   MM   `Mb. YM.     ,M Mb     dM   MM      MM    MM     ,M   MM   `Mb. 
+																AMVmmmmMM .JMMmmmmMMM   `"bmmmd'  .JMML. .JMM. `bmmmmd"' P"Ybmmd"  .JMML.  .JMML..JMMmmmmMMM .JMML. .JMM.
+																									  _                        
+																									 |_   _|_ ._ _. ._   _ _|_ 
+																									 |_ >< |_ | (_| | | (/_ |_  V5 - START -> 08 decembre 2023 
+																											   
+																	******************************************* 
+																									Developed by:  ZeCrusher <zecrusher@gmail.com>  
+																									
+																																										
+																														..                                                   
+																														  .:..                                               
+																															.:o:.                                            
+																															   cc.                                           
+																					..::ccoc:......                             .:..                                         
+																							   :ccoccccc::.                       ....                                       
+																								.. ..CCocc::...                 ...:::.               ..                     
+																									   .oo::::..:               ..:ooc::.              :.                    
+																									  .  oo:::::.::            .::ccoc::  .            .:.                   
+																									 ...:.cCc::.              ...:cc::::.. .            .:.                  
+																									 ...::.Ccc.              ...::oCoc:::. .            .c:.                 
+																									 ...:cc::.               .::::CCo:..               ..c:..                
+																			   .:::::::...            :.:::..                .cccco:..                 ...c:.                
+																		.:c::...:.:::c::.             :ccooc..             :ooCCOOc.                  ....c:..               
+																	:ooo:.::..:c:cccoCOo.             cOO:Oc:.            oOOOOOo. .                 .....:cc.               
+																.:.   .          ...:ooo:             :O88Ococ:....:..::cOOOOCc:.:.                 :::::.CCc.               
+																				  ... .o:            .O88OO88888COO: oOCO:oOO8O8Ooc.              .::cccooCc.. .             
+																					... :c         :co8O8CCoOCCooC: :OoccoocoooCccc::oCc:.:.    :ooccoCCOoc:..         .     
+																					 ....:OC:. .c:c8OCococc.....cc  cc..::::cc::::oC:. cCCCCOOCOOOOCCCOOoc.....        .     
+																					  .:.c8@@8@oOo:Coc:.:. . ...c:.  ... ..:.:..:C:. :o:cCO88OO8OOOCOo:........       ..     
+																					  ..:o8OOCCCoO::c::.    .::::..   ..:c:...:CC:  o:.ccoOCOOOOCc:..                 ..     
+																	  :o:.             :OO8Ooo:..:c: ........             .......  c.. .::::ccooCo:.                 .:.     
+																 ..:ccoOC:           .:OOooccc.. .:..   .                         ...:...:.:::ooc::.               .::c .    
+															   .:::cCCoOOo:        ..oCoo::..... :.                                  .::. .:c::......             .:cc:      
+															 :c:cc::::::...c.      :o.:ooc.. .::..                                    ..::.  .:oCOOo:.          :ccooo.         
+															:cc.:........... :cCCCOoC8o..::...                                             .c::ccO8Ooc... ..:occCOOOo:.         
+														  .:.              ...oOCOOOOococ.  ..                                           .:.:: :COO@88O:cOOO88OCOOCc:..         
+														 :.                  .:CCCooc:. .:.                   ...                         .. ..cCOC.o8oc:..coOC::....           
+														:                    .:OCccc:.. ..                .::...    ......                :: ..:cccooocc::... . ....            
+													   .                     .cCCc:... ::              .:..    .     .   ..                :Cc....cooOc::.. .      .         .  
+																.           .  .:ooCoc..              :..    .  ..      ..:..              .:::coCoOOCCCc.                  ..  
+															  ...c.          oCc:.   .               c:.    ......... .......            ..  ..::.::c.                    ...   
+															 ..:cCOoc.   ..:oCCCoCCc:.              :o. .....:.:.............            .occoCCc:c:::c:...              ::..   
+															 :oooCocc.c:coocCOCCo:cc:              .cc:....::..       ........           .:c:c.   :.:c::.             .::.:. 
+															:C:...  ..... :coooocc:.c.            ..:c:...:c.            ....             o:. .....:oCoo::...     ..:::..:.. 
+														   cc:...       .. .::::c:..:.            ..co...:c:              .              .c. . ..:cooocccccccoCooocc...::.   
+														  ::.               .::...cc.             . co::::c:             .              .cc::::.:::ccco.:oc:oCoocc...:...    
+														 ::.               .:oCoCo:.                .oc::cc:     ZERG RUSH             .cC::cc:::ccccccooooooOOooc:::::.     
+														 :.                    .. . .             .  cooccc:.                          oOoc  .:::c:cccoooCCCoOoc:.:::..      
+														.                   .:oOOoc:...              .:Coc:::                        .cCo:.........:::ccoooccocc::::.        
+																			.coO8o:..              . ..:co....                      :oCo:..........::::ccoooooc::::..        
+																		  :::oCCOOo:..             .... .:coc:..                  .cCoc...........::::coococcccc:..          
+																.cOCccoc..:ccCCocoCo:..             ....   .:cco:.             .cCOCo:............:cccccc:::c:::...          
+																.Coco:. .....  ..:oococ:             .....    :::ocooc:::ccoooCOOo::::...........::::ccccccccc:.             
+																.Co:.           .:ooo:.               ....   .........:::c:ccc:::.............::.::ccooococcc...             
+															   .oc:.               .oCOC:. .:       .    ..   . ...   .    . . ............::::::cccooooocc:..               
+															   :c.               .:::.::::.               ......... . ..    ...  .......::.:::ccoooooccc::.                  
+															   .:.                .::c:.:c                   ................... ......:::::cccccoCoocc:..                   
+															   ..                ccoccc:.:c.                   ....:. .....:.......:....::ccccocoCCoc:..                     
+															   ..              .::CCCCocc:.::.                     .::.::::::::::..::..:c:cccooCCoo:..                       
+																.       .  ..:c::......:c::.co:.         :.           ..:::::::::::.:cocccoococcc::.                         
+																	.        .::::..........  ...ooCo:                         ..:.::::c::c:ccoooc:...                           
+																		..:c:           ..  .cc:.:oc                          .....:::::::c:::.                              
+																		  :c.               .  :cc::cc.                 .             ...... .                               
+																		  :o.                 . ...  .cc..                                                                   
+																		  .:..                  :c:..   .:::.                                                                
+																			:..                 :c:...    ..:::..:                      .                                    
+																			 .:                 ccc:::..     .::cc...                                                        
+																   .                :ccc:::::..    ..:::c::.                    
+																				   .cc:::..:.:::.     .:::...:....             
+																				  .coc::..........    :ccc:....                
+														  .                     ..ccc::.........    .:cc:...                   
+															.:..              .:::::.:.............::.....                    
+															   .:..........:::..............:::::...                          
+																  :::.:.....::....::::::ccoo:..                               
+																	   .::::::::c:::.. ...                                     
+
 									
 */
 
